f05822bc05148a8f00fde2b91fac4e8d3e27f478
Yolijn <yolijn@frameless.io>
2021-04-16 14:49:47 +0200

    Make legend optional

--- src/App.tsx
+++ src/App.tsx
@@ -4,6 +4,7 @@ import skills from './data/skills.json';
 import selfAssessment from './data/self-assessment.json';
 import { SkillSubject } from './SkillSubject';
 import { Legend } from './Legend';
+import { useState } from 'react';
 
 const assessment = selfAssessment.reduce((obj, item) => {
   obj[item.id] = item;
@@ -11,10 +12,29 @@ const assessment = selfAssessment.reduce((obj, item) => {
 }, {} as { [key: string]: SkillAssessment });
 
 const App = () => {
+  const [legendOpen, setLegendState] = useState(false);
+  const toggleLegend = () => setLegendState(!legendOpen);
+  const closeLegend = () => setLegendState(false);
+
   return (
     <div className="content content--small-print content--centered print--new-page">
-      <h2 id="self-assessment">Self assessment</h2>
-      <Legend data={skillLevels} title="Legend: Levels of proficiency" />
+      <header className="app-header">
+        <h2>Self assessment</h2>
+        <button
+          onClick={toggleLegend}
+          title="Open Legend"
+          className="button button--icon"
+        >
+          ⓘ
+        </button>
+      </header>
+      {legendOpen && (
+        <Legend
+          data={skillLevels}
+          title="Legend: Levels of proficiency"
+          closeLegend={closeLegend}
+        />
+      )}
       <div className="skills">
         <h2 className="hide-from-view">Skills</h2>
         {skills.map((skill) => (
--- src/Legend.tsx
+++ src/Legend.tsx
@@ -1,13 +1,17 @@
 interface LegendProps {
   data: { value: string; description: string }[];
   title: string;
+  closeLegend: () => void;
 }
 
-export const Legend = ({ data, title }: LegendProps) => (
+export const Legend = ({ data, title, closeLegend }: LegendProps) => (
   <div className="legend">
     <div className="legend__content">
       <div className="legend__header">
         <h3>{title}</h3>
+        <button onClick={closeLegend} className="button button--icon">
+          ╳
+        </button>
       </div>
       <dl className="legend__list">
         {data.map((item) => (
